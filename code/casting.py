27c8c7f05d61fa7a016eabae740bd56f9ef17066
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-09 22:40:42 +0100

    Added a python file for type casting

---
+++ code/casting.py
@@ -0,0 +1,23 @@
+# working with casting 
+# casting is used to convert a datatype to another datatype 
+
+number_of_cars = "23"
+amount_per_car = 4000
+
+# casting number_of_cars to int. 
+sale = int(number_of_cars) * amount_per_car
+print(sale)
+
+
+float_sale = float(sale)
+
+
+print(float_sale) # printing sale 
+print(type(float_sale)) # printing the datatype 
+
+number = 23
+str_num = str(number) # casting an int to string  
+
+# printing the type 
+print(type(number)) 
+print(type(str_num))
