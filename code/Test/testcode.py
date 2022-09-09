51842c76b5de2accd581be744a0dc18b1d7778b4
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-09 22:28:18 +0100

    tested lists

---
+++ code/Test/testcode.py
@@ -0,0 +1,22 @@
+# testing some list methods here
+home = ["mahmud", 2, "mama", 6, "easy", 3, "grandma", 8 , 8 , 67, 8]
+print(home.index("mama")) # print the index of the item in the method
+print(f'The number 8 occurs: {home.count(8)} times')
+
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
+
+
+
