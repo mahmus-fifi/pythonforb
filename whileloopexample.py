b95f8de7890327e92aba5c4fa15190a2c800a6e2
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-16 22:05:14 +0100

    added whileloop example

---
+++ whileloopexample.py
@@ -0,0 +1,26 @@
+"""
+while loop syntax 
+while keyword 
+condition :
+indent the next block of code 
+"""
+"""number = 0 
+while number < 4:
+    print("Hello there")"""
+    
+"""
+name = "" # empty string variable 
+while name != "your name": # != not operator 
+    print("kindly enter your name")
+    name = input()
+print("thanks for entering your name")
+"""
+
+# using the break statement 
+
+while True:
+    print("please enter username: ")
+    user_name = input()
+    if user_name == 'mahmus':
+        break
+    print("thanks for entering your name")
