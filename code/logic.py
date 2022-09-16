6c4f101dc111d4e07a2e24e59b2d705f14a1e211
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-16 08:25:07 +0100

    working with if and elif statements

---
+++ code/logic.py
@@ -0,0 +1,24 @@
+# working with logical operators 
+# syntax 
+"""
+if expression:
+    statement
+"""
+"""# creating an age variable 
+age = int(input("Please enter your age: "))
+if age < 10:
+    print("hello kiddo")
+elif age > 10:
+    print("Hi there you are older than ten")
+ """
+day = input("Enter the day of the week: ") 
+# convert day to lowercase 
+day_low = day.lower()
+
+# code
+if (day_low == "monday"):
+    print("its going to be busy a today!")
+elif (day_low == "friday"):
+    print("the weekend is here")
+    
+
