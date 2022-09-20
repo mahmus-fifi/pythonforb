e8e7b5b8de57c32ec4cce87e64532e02dff0d1f1
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-20 09:49:18 +0100

    reorganised the project files, deleted unnecessary files, renames files

--- code/README.md
+++ code/README.md
@@ -1,5 +1,21 @@
 # Python Programmming Course for Beginners
 
+## Python for Beginners :smile:
+
+## What to expect from this course
+
+This course is a free course that would improve you knowledge on basic python to intermediate programming skills using 
+Visual Studio Code Building interractive programs
+
+## Materials you need
+
+You can follow along using a mobile device or tablet, but a laptop or desktop is preferrable
+
+### Software :cd:
+
+I will be using visual studio code but other editing programs (IDE's) should be excellent.
+You will find all folders and code associated with this class. you can also access the playlist here:
+
 ## A practical Approach to Learning Python Programming
 
 This project is a free course on python programming for beginners. You can find the full playlist here [Python for Beginners](https://www.youtube.com/watch?v=dm-OmwUrPMY&list=PLrSslYDiIROWR01WzFiSR5JFWAC0r85of)
@@ -11,4 +27,5 @@ This project is a free course on python programming for beginners. You can find 
 3. Casting
 4. Formatspecifiers
 5. Formatspecifiers 2
-
+6. Using WhileLoop
+7. Python Lists Tuples and Dictionaries
--- code/Test/lists.py
+++ code/Test/lists.py
@@ -1,3 +1,5 @@
+"""Working with list methods"""
+
 # testing some list methods here
 home = ["mahmud", 2, "mama", 6, "easy", 3, "grandma", 8 , 8 , 67, 8]
 print(home.index("mama")) # print the index of the item in the method
@@ -17,6 +19,49 @@ print(place_up)
 
 # Print out the number of o's in place
 print(place.count("o"))
+
+
+# working with lists 
+"""our_list = []
+# adding data to a list 
+our_list.append("ps5")
+our_list.append("xbox1 s")
+our_list.append(230.34)
+# muting objects in a list 
+our_list[0] = 'ps4'
+# setting a float value for the second index in our_list 
+our_list[1] = 500.67
+# removing elements in a list using del 
+#del our_list[0]
+print('items in the list :%r' %our_list)
+"""
+"""items = ['God of war', 'Resident Evil 3','zelda' ,'Tekken Tag Tournament','Ace Combat']
+sorted_items = sorted(items)
+print(f'Items in the list are: {sorted_items}')
+items.pop(0)
+print(items)
+"""
+
+# test sorting
+numbers = [1,3,5,2,1,5,4,2,2,1,1,1,]
+numbers.sort()
+print(f'Listing the Items in the list:{numbers}')
+
+"""
+we haven't changed (mutated) any items in the list
+if we do and try to print it, python will return NONE. none is a python object
+that tells us that the object dosent have a datatype that represents it. when we mutate a list 
+using list methods such as append() or sort(), Python knows that we have changed the list and to prevent 
+errors it assigns a value  'None' to the new mutated list to prevent reference conflict from the original list.
+
+name = 'string'
+name = ''
+name = None 
+
+let's mutate the list and see what happens 
+
+can we create an empty list and store the reference of the mutated list to prevent seeing None as our output? :smile: let's try 
+"""
 
 
 
