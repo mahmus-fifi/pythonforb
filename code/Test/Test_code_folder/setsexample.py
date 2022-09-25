aad9eeb2338d33977779dbbeae217dbd8937ae1e
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-25 10:01:34 +0100

    completed the set example

--- code/Test/Test_code_folder/setsexample.py
+++ code/Test/Test_code_folder/setsexample.py
@@ -19,6 +19,46 @@ print(type(cat_names))
 number_of_cats = len(cat_names)
 print(f"we have a total of {number_of_cats} cat's in this house")
 
+# cat ages 
+cat_ages = set((1,3,2,1,3,)) # a set of cat ages notice the double parenthesis, don't confuse with tuples
+print(cat_ages)
+
+# Accessing Sets
+## note that we can't use index to access sets 
+# error below
+#print(cat_names[0])
+
+# check if an item exists in the set using the built in keyword in
+check = "GabbyCat" in cat_names
+print(type(check),check)
+print(f'Datatype of check is: {type(check)}, and is there "GabbyCat" in cat_names? {check}')
+
+# loop through items 
+for items in cat_names:
+    print(items)
+    
+# adding items to sets 
+cat_names.add("CupCakeCat")
+print(cat_names)
+
+
+# adding items from another set 
+cat_names.update(cat_ages)
+print(cat_names)
+
+# adding lists
+lst_days = ["mon","tue","wed","thurs"]
+cat_names.update(lst_days)
+print(cat_names)
+
+# discard items in a set 
+cat_names.remove("GabbyCat")
+print(cat_names)
+
+#print(help(set))
+#cat_names. on vs code
+
+
 
 
 
